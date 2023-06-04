82663da6e99d56c5f939abbf02ea3a4138628019
ZmeykaTatiana <zacharovanaja@mail.ru>
2023-06-04 20:45:38 +0300

    first

---
+++ src/main/java/com/example/demo0406/Demo0406Application.java
@@ -0,0 +1,13 @@
+package com.example.demo0406;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo0406Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo0406Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo0406/FirstController.java
@@ -0,0 +1,15 @@
+package com.example.demo0406;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello AGAIN";
+    }
+}
