43b6b1877c6cf38cec995e86b8f8181ee845dc36
ZmeykaTatiana <zacharovanaja@mail.ru>
2023-06-04 23:02:01 +0300

    the last one

--- src/main/java/com/example/demo0406/Controller/FirstController.java
+++ src/main/java/com/example/demo0406/Controller/FirstController.java
@@ -1,15 +1,16 @@
-package com.example.demo0406;
+package com.example.demo0406.Controller;
 
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class FirstController {
 
 
     @GetMapping("/hello")
     public String hello(){
-        return "hello";
+        return "hello1";
     }
 }
